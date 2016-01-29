3a41460716f69f036571113fb8f9f35ecc4908a3
Michal Parusinski <mparusinski@gmail.com>
2016-01-29 22:12:55 +0100

    Streamlined a bit more the automake setup

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-
-test -z "$srcdir" && srcdir=$(dirname "$0")
-test -z "$srcdir" && srcdir=.
-
-cwd=$(pwd)
-cd "$srcdir"
-
-autoreconf --verbose --force --install || exit $?
-
-cd "$cwd"
-"$srcdir/configure" $@
+aclocal
+autoreconf
+autoheader
+automake --add-missing
