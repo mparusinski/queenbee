cf320b26d04ed7d575aa59b9067421f1ad52553d
Michal Parusinski <mparusinski@gmail.com>
2016-01-30 14:19:47 +0100

    Basic usage: Using getopt_long instead of regular getopt

--- src/queenbee.c
+++ src/queenbee.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
 
 #include "common.h"
 
 #define OPTSTR "vh"
 
 typedef struct {
+  char * executable_path;
 } args_t;
 
 void init_args( args_t * arguments ) {
+  arguments->executable_path = NULL;
 }
+
+struct option long_opts[] = {
+  { "help", no_argument, NULL, 'h' },
+  { "version", no_argument, NULL, 'v' },
+  { NULL, required_argument, NULL, 0 }
+};
 
 void display_version( void ) {
   char str[32];
@@ -19,22 +28,23 @@ void display_version( void ) {
 }
 
 void display_usage( void ) {
-  puts( "Usage: " NAME " [executable]");
+  puts( "Usage: " NAME " [FILE]");
   puts(  NAME " is an application wrapper which automatically detects \n"
         "integrated / dedicated GPU setups and executes application \n"
         "against best GPU" );
   puts( "" );
-  puts( "-h      displays this help section" );
-  puts( "-v      displays the version number");
+  puts( "-h, --help         displays this message" );
+  puts( "-v, --version      displays the version number");
 }
 
 int main(int argc, char *argv[]) {
   args_t arguments;
+  int long_index = 0;
   int opt = 0;
 
   init_args(&arguments);
 
-  opt = getopt( argc, argv, OPTSTR );
+  opt = getopt_long( argc, argv, OPTSTR, long_opts, &long_index  );
   while ( opt != -1 ) {
     switch ( opt ) {
       case 'v':
@@ -43,9 +53,10 @@ int main(int argc, char *argv[]) {
       case 'h':
         display_usage();
         return EXIT_SUCCESS;
-      default:
-        display_usage();
-        return EXIT_SUCCESS;
+      case 0:
+        return EXIT_FAILURE;
+      default: // Should not get here
+        return EXIT_FAILURE;
     }
   }
 
